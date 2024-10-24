9bb6b82e76564d262883ef6f4de65bcad93e9be8
Murilo Cardoso S. Appugliese <148897349+murilocapp@users.noreply.github.com>
2024-10-24 00:56:28 +0000

    fix

--- lab03/flash.sh
+++ lab03/flash.sh
@@ -1,4 +1,4 @@
-#!/ bin / bash
+#!/bin/bash
 2
 3 # script baseado no có digo disponibilizado em:
 4 # Derek Molloy , Exploring Raspberry Pi: Interfacing to the Real World with Embedded Linux ,
@@ -6,13 +6,13 @@
 6 #
 7 # Script usado para chavear um pino da GPIO na frequencia mais alta
 8 # possivel usando Bash
-9 echo 23 > / sys / class / gpio / export
+9 echo 23 > /sys/clas/gpio/export
 10 sleep 0.5
-11 echo " out" > / sys / class / gpio / gpio23 / direction
+11 echo " out" > /sys/class/gpio/gpio23/direction
 12 COUNTER =0
 13 while [ $COUNTER -lt 20000 ]; do
-14 echo 1 > / sys / class / gpio / gpio23 / value
+14 echo 1 > /sys/class/gpio/gpio23/value
 15 let COUNTER = COUNTER +1
-16 echo 0 > / sys / class / gpio / gpio23 / value
+16 echo 0 > /sys/class/gpio/gpio23/value
 17 done
-18 echo 23 > / sys / class / gpio / unexport
+18 echo 23 > /sys/class/gpio/unexport
