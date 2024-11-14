5c4b9ebc3ebdff45532353ddf34ba77c15bfda02
Murilo <bla@oi.com>
2024-11-13 23:45:36 -0200

    Agora sem ponto

--- lab05/Teste_5.c
+++ lab05/Teste_5.c
@@ -12,11 +12,11 @@ int main() {                            // este programa deve ser rodado com 'su
 
    // Ajustando a frequencia do PWM em 10kHz com 128 passos de duty cycle
    // frequencia PWM = 19,2 MHz / (divisor * range)
-   frequencia = 19200000 / (573.39 * 128);
+   frequencia = 19200000 / (573 * 128);
 
    pwmSetMode(PWM_MODE_MS);             // usando frequencia fixa
    pwmSetRange(128);                    // (range) passos do duty cycle (max=4096)
-   pwmSetClock(573.39);                    // (divisor) fornece uma frequencia de 10kHz (max=4096)
+   pwmSetClock(573);                    // (divisor) fornece uma frequencia de 10kHz (max=4096)
    printf("Iniciando...\n");
    for(ciclos = 0; ciclos < 4; ciclos++){  // variando o duty cycle
          pwmWrite(pino_PWM0, 64);
