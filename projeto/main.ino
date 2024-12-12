3f07d59524c3e5427ed2df8d4ad99ba4c66cfc92
murilocapp <mu.appugliese@gmail.com>
2024-12-12 00:59:54 -0300

    padrão de led para alarme

--- projeto/main.ino
+++ projeto/main.ino
@@ -2,13 +2,23 @@ const int ledPin = 2; //pino digital de saida do led
 const int micPin = A0; //pino do microfone 
 int ledState = 0; //inicia com led desligado
 int iniciaColeta = 0; //variavel de estado da coleta
-int freqAquisicao = 1; //em Hz
+int loopDelay = 1000;
 char charRecebido;
 
+void alarm(int bips = 4, int interval = 100) {
+    for (int i = 0; i < bips; i++) {
+        digitalWrite(ledPin, HIGH); // Liga o LED
+        delay(interval);           // Aguarda um intervalo
+        digitalWrite(ledPin, LOW); // Desliga o LED
+        delay(interval);           // Aguarda outro intervalo
+    }
+}
+
+
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(micPin, INPUT);
-  Serial.begin(115200, SERIAL_8N1);
+    pinMode(ledPin, OUTPUT);
+    pinMode(micPin, INPUT);
+    Serial.begin(115200, SERIAL_8N1);
 }
 
 void loop(){
@@ -38,9 +48,9 @@ void loop(){
         Serial.write((valorSensor >> 8)); //byte menos significativo
     }
     if(ledState == 1){
-        
+        alarm(4, loopDelay/10)
     }
 
 
-  delay(freqAquisicao*1000);
+  delay(loopDelay);
 }
