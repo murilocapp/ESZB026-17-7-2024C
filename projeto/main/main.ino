3dd2c663622db0b7ac0c147def8268291899dcc5
Murilo <bla@oi.com>
2024-12-16 22:00:38 -0200

    acabalogo

---
+++ projeto/main/main.ino
@@ -0,0 +1,67 @@
+const int ledPin = 2; //pino digital de saida do led
+const int micPin = A0; //pino do microfone 
+int ledState = 0; //inicia com led desligado
+int printSerial = 0; //utilizado para debug
+int iniciaColeta = 0; //variavel de estado da coleta
+int loopDelay = 10;
+char charRecebido;
+
+void alarm(int bips = 4, int interval = 100) {
+    for (int i = 0; i < bips; i++) {
+        digitalWrite(ledPin, HIGH); // Liga o LED
+        delay(interval);           // Aguarda um intervalo
+        digitalWrite(ledPin, LOW); // Desliga o LED
+        delay(interval);           // Aguarda outro intervalo
+    }
+}
+
+
+void setup() {
+    pinMode(ledPin, OUTPUT);
+    pinMode(micPin, INPUT);
+    Serial.begin(115200, SERIAL_8N1);
+}
+
+void loop(){
+    if (Serial.available() ) {
+       charRecebido = Serial.read();
+       switch (charRecebido) {
+         case 'l':
+           ledState = 1;
+           break;
+         case 'd':
+           ledState = 0;
+           break;
+         case 'i':
+           iniciaColeta = 1;
+           break;
+         case 'p':
+           iniciaColeta = 0;
+           break;
+         case 'b': //debug
+           printSerial =1;
+           break;
+         case 'm': //debug
+           printSerial =0;
+           break;
+        default: 
+            break;
+        
+       }
+    }
+    if(iniciaColeta == 1){
+        int valorSensor = analogRead(micPin);
+        if(printSerial == 1){
+          Serial.println(valorSensor);
+          }else{
+            Serial.write(valorSensor & 0xFF); //byte mais significativo
+            Serial.write((valorSensor >> 8));//byte menos significativo
+            }
+    }
+    if(ledState == 1){
+        alarm(4, loopDelay/10);
+    }
+
+
+  delay(loopDelay);
+}
