a97b4e6e5d49798ee61aa41b492995cad95e4197
Murilo <murilo.appugliese@einstein.br>
2024-12-18 18:26:51 -0300

    teste

--- projeto/app.py
+++ projeto/app.py
@@ -142,7 +142,7 @@ def update():
 
         # Atualiza o gráfico
         data1[x_atual] = novodado
-        data1[(x_atual + 1) % npontos] = np.nan
+        #data1[(x_atual + 1) % npontos] = np.nan
         x_atual = x_atual + 1
         if x_atual >= npontos:
             x_atual = 0
